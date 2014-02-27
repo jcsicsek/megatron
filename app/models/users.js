27b1888851b9d629af9869ada62ed39beb9aeaa0
Jeff <jcsicsek@gmail.com>
2014-02-26 22:14:56 -0500

    passport authentication, swig template engine

--- app/models/users.js
+++ app/models/users.js
@@ -1,6 +1,24 @@
 module.exports.create = function(pgClient) {
   var self = {
-
+      getByEmail: function(email, callback) {
+        var query = "SELECT * FROM users WHERE email=?";
+        pgClient.query(query, [email], function(error, result) {
+          callback(error, result.rows[0]);
+        });
+      },
+      create: function(email, passwordHash, callback) {
+        var now = new Date();
+        var query = "INSERT INTO users (email, password, created, modified, last_login, active) VALUES (?, ?, ?, ?, ?, ?)";
+        pgClient.query(query, [email, passwordHash, now, now, now, true], function(error, result) {
+          callback(error, result);
+        });
+      },
+      get: function(userId, callback) {
+        var query = "SELECT * FROM users WHERE id=?";
+        pgClient.query(query, [userId], function(error, result) {
+          callback(error, result.rows[0]);
+        });
+      }
     }
   return self;
 }
--- app/server.js
+++ app/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var pg = require('pg');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var swig = require('swig');
 
 var pgConnString = process.env.DATABASE_URL;
 var port = process.env.PORT || 5000;
@@ -8,11 +11,49 @@ var pgClient = new pg.Client(pgConnString);
 pgClient.connect();
 
 var usersController = require('./controllers/users').create(pgClient);
+var usersModel = require('./models/users').create(pgClient);
+var passwordHash = require('password-hash');
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  usersModel.get(id, function (error, user) {
+    done(error, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(email, password, done) {
+    usersModel.getByEmail(email, function(error, user) {
+      if (error) {
+        done(error);
+      } else if (!user) {
+        done(null, false, { message: 'Unknown user ' + email });
+      } else if (!passwordHash.verify(password, user.password) {
+        done(null, false, { message: 'Invalid password' });
+      } else{
+        done(null, user);    
+      }
+    });
+  }
+));
 
 var app = express();
 
+//Setup Swig template engine
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views/');
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
+
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.session({ secret: 'poop' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/users/login', usersController.loginPage);
 app.post('/users/login', usersController.login);

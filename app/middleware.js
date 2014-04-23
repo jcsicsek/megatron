9cb20ab9522f306c90345e12a6625a3f61a1c3d0
jcsicsek <jcsicsek@gmail.com>
2014-04-22 22:33:20 -0400

    added color theme customization (DB)

--- app/middleware.js
+++ app/middleware.js
@@ -20,6 +20,7 @@ module.exports.create = function(pgClient) {
       if (req.subdomains.length == 1) {
         usersModel.getPartner(req.subdomains[0], function(error, merchant) {
           res.locals.logoUrl = merchant.logo_url;
+          res.locals.colorTheme = merchant.color_theme;
           next();
         });
       } else {
---
+++ db_migrations/add_color_theme_to_partners.sql
@@ -0,0 +1,1 @@
+ALTER TABLE partners ADD COLUMN color_theme TEXT;

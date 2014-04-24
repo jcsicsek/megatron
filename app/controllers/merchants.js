cfd0d1bd22796f41c98147dc6e46579ce7bb31f3
jcsicsek <jcsicsek@gmail.com>
2014-04-24 17:02:04 -0400

    passed merchant loans to merchant overview and merchant invoice list pages, templates still need modification

--- app/controllers/merchants.js
+++ app/controllers/merchants.js
@@ -1,10 +1,16 @@
+var sf = require('../lib/salesforce');
+
 module.exports.create = function() {
   var self = {   
     overview: function(req, res) {
-      res.render('manage/merchant/overview', { title: "Your Store Overview | tabb.io" });
+      sf.queryByMerchant(req.subdomains[0], function(error, loans) {
+        res.render('manage/merchant/overview', { title: "Your Store Overview | tabb.io", loans: loans });
+      });
     },
     invoices: function(req, res) {
-      res.render('manage/merchant/invoices', { title: "Your Store Invoices | tabb.io" });
+      sf.queryByMerchant(req.subdomains[0], function(error, loans) {
+        res.render('manage/merchant/invoices', { title: "Your Store Invoices | tabb.io", loans: loans });
+      });
     },
     settings: function(req, res) {
       res.render('manage/merchant/settings', { title: "Your Store Settings | tabb.io" });
